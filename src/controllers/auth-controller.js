559ac11403f590732a8ed7d9bfc4de7f8f3c4835
mungsehari <mungse.hari@gmail.com>
2024-07-14 23:43:35 +0530

    login add

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -18,6 +18,18 @@ class AuthController {
     }
     return res.json({ massage: "Otp" });
   }
+  login(req, res) {
+    const { email, password } = req.body;
+    const schema = Joi.object({
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).required(),
+    });
+    const { error } = schema.validate(req.body);
+    if (!!error)
+      return res.status(400).json({ error: error.details[0].message });
+
+    return res.json({ message: "Logged in successfully" });
+  }
 }
 
 module.exports = AuthController;
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,5 @@ const AuthController = require("./controllers/auth-controller");
 const authController = new AuthController();
 
 router.post("/auth/sign-up", authController.signUp);
-
+router.post("/auth/login", authController.login);
 module.exports = router;
