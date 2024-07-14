f8f3dd1694d6b7f2a2568b56bf79edbfb7ca57d5
mungsehari <mungse.hari@gmail.com>
2024-07-14 23:33:57 +0530

    import routes

---
+++ src/controllers/auth-controller.js
@@ -0,0 +1,23 @@
+const Joi = require("joi");
+
+class AuthController {
+  signUp(req, res) {
+    const { name, email, password, confirmPassword } = req.body;
+    const schema = Joi.object({
+      name: Joi.string().min(3).required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).required(),
+      confirmPassword: Joi.ref("password"),
+    });
+    const { error } = schema.validate(req.body);
+    if (!!error)
+      return res.status(400).json({ error: error.details[0].message });
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords do not match." });
+    }
+    return res.json({ massage: "Otp" });
+  }
+}
+
+module.exports = AuthController;
